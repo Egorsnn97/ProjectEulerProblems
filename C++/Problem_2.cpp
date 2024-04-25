7571ce1075a8c68acf02d78e636a23120d9fa9bd
Egorsnn97 <168059800+Egorsnn97@users.noreply.github.com>
2024-04-25 19:52:03 +0300

    Create Problem_2.cpp

---
+++ C++/Problem_2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+int Problem_2() {
+	int a = 1, b = 2,c,sum = 0;
+	while (b < 4000000) {
+		if (b % 2 == 0) {
+			sum += b;
+		}
+		c = a;
+		a = b;
+		b += c;
+	}
+	return sum;
+}
+
+int main() {
+	
+	cout << Problem_2() << endl;
+
+	return 0;
+}
+//4613732
