9ffbd64c0a275d4730d2f43281ad572213a14546
Egorsnn97 <168059800+Egorsnn97@users.noreply.github.com>
2024-04-25 09:45:36 +0300

    Update Problem_2.py

--- Python/Problem_2.py
+++ Python/Problem_2.py
@@ -1,10 +1,11 @@
-summ = 0
-a = 1
-b = 2
-while(b < 4000000):
-    if(b % 2 == 0):
-        summ+=b
-    a,b=b,a+b
-print(summ)
+def Problem_2():
+    summ = 0
+    a = 1
+    b = 2
+    while(b < 4000000):
+        if(b % 2 == 0):
+            summ+=b
+        a,b=b,a+b
+    return summ
+print(Problem_2())
 #ANSWER: 4613732
-#Problem2
