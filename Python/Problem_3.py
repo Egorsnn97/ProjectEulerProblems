f36674f4d94226c4ea5fb4c32d91afe995fd6d77
EgorSonin <168059800+Egorsnn97@users.noreply.github.com>
2024-04-25 20:50:03 +0300

    Update Problem_3.py

--- Python/Problem_3.py
+++ Python/Problem_3.py
@@ -1,1 +1,15 @@
-
+num = 600851475143
+def IsPrime(i):
+    for j in range(2,i//2):
+        if(i % j == 0):
+            return 0
+def Problem_3():
+    global num 
+    while(num != 1):
+        for i in range(2,num):
+            if(num % i == 0 and IsPrime(i) != 0):
+                num /= int(i)
+            if(num == 1):
+                return i
+print(Problem_3())
+#6857
